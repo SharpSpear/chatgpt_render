91a081cb23d167a01d4e23bb3ae8033e75cdadfd
Maksim Kholin <105179860+SharpSpear@users.noreply.github.com>
2023-05-10 00:33:36 +0900

    Update register.js

--- client/src/Components/page/register.js
+++ client/src/Components/page/register.js
@@ -200,7 +200,7 @@ export default function Register() {
 					</Grid>					
 					<Grid container justify="flex-end">
 						<Grid item>
-							<Link href="/login" variant="body2">
+							<Link to="/login" variant="body2">
 								Already have an account? Sign in
 							</Link>
 						</Grid>
